c79aeb5c536eafe2bffe2035ec29dfab23779725
Denis Gabriel VIERIU <denisvieriu925@gmail.com>
2018-11-23 00:19:57 +0200

    Teacher can add grades to students

--- FrontEnd/src/app/_modules/teacher-main-page/teacher-main-page-routing.module.ts
+++ FrontEnd/src/app/_modules/teacher-main-page/teacher-main-page-routing.module.ts
@@ -1,19 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { TeacherMainPageComponent } from './pages/teacher-main.page';
-import { AttendancesComponent } from './components/attendances/attendances.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {TeacherMainPageComponent} from './pages/teacher-main.page';
+import {AttendancesComponent} from './components/attendances/attendances.component';
+import {GradesComponent} from './components/grades/grades.component';
 
 
 const routes: Routes = [
-    {path: '', component: TeacherMainPageComponent}
+  {path: '', component: TeacherMainPageComponent},
+  {path: 'grades', component: GradesComponent},
 ];
+
 @NgModule({
-    imports: [
-        RouterModule.forChild(routes),
-    ],
-    exports: [
-        RouterModule
-    ]
+  imports: [
+    RouterModule.forChild(routes),
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class TeacherMainPageRoutingModule {
 }
