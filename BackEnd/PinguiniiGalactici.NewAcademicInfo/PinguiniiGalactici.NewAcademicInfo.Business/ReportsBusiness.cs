678a474a32410b6a278d3d071414ed8717cc9857
Vlad <nvie2185@scs.ubbcluj.ro>
2019-01-11 03:09:29 +0200

    Reports are uploaded from the DB to the Backend + created API endpoints for the two functional reports

---
+++ BackEnd/PinguiniiGalactici.NewAcademicInfo/PinguiniiGalactici.NewAcademicInfo.Business/ReportsBusiness.cs
@@ -0,0 +1,28 @@
+﻿using PinguiniiGalactici.NewAcademicInfo.Business.Core;
+using PinguiniiGalactici.NewAcademicInfo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinguiniiGalactici.NewAcademicInfo.Business
+{
+    public class ReportsBusiness: BusinessObject
+    {
+        #region Constructor
+        public ReportsBusiness(BusinessContext context) : base(context) { }
+        #endregion
+
+        #region Methods
+        public IEnumerable<AverageGradeReport> AverageGradesReport(Guid CourseID, String TypeName)
+        {
+            return _context.DALContext.ReportsDAL.AverageGradesReport(CourseID, TypeName);
+        }
+        public IEnumerable<PassingGradesReport> PassingGradesReport(Guid CourseID, String TypeName)
+        {
+            return _context.DALContext.ReportsDAL.PassingGradesReport(CourseID, TypeName);
+        }
+        #endregion
+    }
+}
---
+++ BackEnd/PinguiniiGalactici.NewAcademicInfo/PinguiniiGalactici.NewAcademicInfo.DAL/ReportsDAL.cs
@@ -0,0 +1,31 @@
+﻿using PinguiniiGalactici.NewAcademicInfo.DAL.Core;
+using PinguiniiGalactici.NewAcademicInfo.Library;
+using PinguiniiGalactici.NewAcademicInfo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinguiniiGalactici.NewAcademicInfo.DAL
+{
+    public class ReportsDAL: DALObject
+    {
+        #region Constructors
+        public ReportsDAL(DALContext context) : base(context) { }
+        #endregion
+
+        #region Methods
+        public IEnumerable<AverageGradeReport> AverageGradesReport(Guid CourseID, String TypeName)
+        {
+            return DbOperations.ExecuteQuery<AverageGradeReport>(_context.CONNECTION_STRING, "dbo.Get_Average_Grade_For_Course_by_Attendance_Type", new SqlParameter("CourseID", CourseID), new SqlParameter("TypeName", TypeName));
+        }
+
+        public IEnumerable<PassingGradesReport> PassingGradesReport(Guid CourseID, String TypeName)
+        {
+            return DbOperations.ExecuteQuery<PassingGradesReport>(_context.CONNECTION_STRING, "dbo.Get_Passing_Grades_Number_For_Course_by_Attendance_Type", new SqlParameter("CourseID", CourseID), new SqlParameter("TypeName", TypeName));
+        }
+        #endregion
+    }
+}
